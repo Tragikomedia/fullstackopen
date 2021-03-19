c580a4e115e66611b0ed53c599d6e22b3336621a
Tragikomedia <kamilk9625@gmail.com>
2021-03-19 18:36:50 +0100

    Finished 1.8 and 1.9.

--- part_1/unicafe/src/Statistics.js
+++ part_1/unicafe/src/Statistics.js
@@ -2,8 +2,22 @@ import Stat from "./Stat";
 
 const Statistics = ({ data }) => {
   const all = data.reduce((acc, stat) => acc + stat.number, 0);
-  const average = data.reduce((acc, stat) => acc + (stat.value * stat.number), 0) / all;
-  const positive = (data.filter((stat) => stat.value > 0).reduce((acc, stat) => acc + stat.number, 0) / all) * 100 + "%";
+  if (!all)
+    return (
+      <>
+        <h2>Statistics</h2>
+        <p>No feedback given</p>
+      </>
+    );
+  const average =
+    data.reduce((acc, stat) => acc + stat.value * stat.number, 0) / all;
+  const positive =
+    (data
+      .filter((stat) => stat.value > 0)
+      .reduce((acc, stat) => acc + stat.number, 0) /
+      all) *
+      100 +
+    "%";
   const statData = [
     ...data,
     { text: "all", number: all },
