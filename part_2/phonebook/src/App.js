e70e9fa965c600e098bff3989f06f9ee7a8cd2ed
Tragikomedia <kamilk9625@gmail.com>
2021-03-20 12:39:42 +0100

    Finished 2.6.

---
+++ part_2/phonebook/src/App.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const addContact = (event) => {
+    event.preventDefault();
+    setPersons([...persons, { name: newName }]);
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addContact}>
+        <div>
+          name:{" "}
+          <input type="text" value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
