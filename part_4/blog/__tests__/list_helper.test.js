5f2f1d03b56e74a9b99701f72e2a6654c2351c7f
Tragikomedia <kamilk9625@gmail.com>
2021-03-26 21:34:09 +0100

    Finished 4.2.

--- part_4/blog/__tests__/list_helper.test.js
+++ part_4/blog/__tests__/list_helper.test.js
@@ -1,9 +1,36 @@
 const listHelper = require('../utils/list_helper');
+const Blog = require('../models/blog');
 
-describe('List helpers', () => {
+describe('Dummy', () => {
   it('Given an array of blogs, dummy should return 1', () => {
     const blogs = [];
     const result = listHelper.dummy(blogs);
     expect(result).toBe(1);
+  });
+});
+
+describe('Total likes', () => {
+  it('Given an empty array, should return zero', () => {
+    const blogs = [];
+    const result = listHelper.totalLikes(blogs);
+    expect(result).toBe(0);
+  });
+  it('Given an array with 1 blog, should return its likes', () => {
+    const likes = 7;
+    const blogs = [
+      new Blog({ title: 'Title', author: 'Author', url: 'url.com', likes }),
+    ];
+    const result = listHelper.totalLikes(blogs);
+    expect(result).toBe(likes);
+  });
+  it('Given an array with many blogs, should return total number of likes', () => {
+    const likes = [2, 4, 27];
+    const total = likes.reduce((acc, like) => acc + like);
+    const blogs = likes.map(
+      (like, index) =>
+        new Blog({ title: 'Title' + index, author: 'Author' + index, url: 'url.com' + index, likes: like })
+    );
+    const result = listHelper.totalLikes(blogs);
+    expect(result).toBe(total);
   });
 });
--- part_4/blog/utils/list_helper.js
+++ part_4/blog/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   if (blogs || !blogs) return 1;
 };
 
+const totalLikes = (blogs) => {
+  const reducer = (acc, blog) => acc + blog.likes;
+  const total = blogs.reduce(reducer, 0);
+  return total;
+};
+
 module.exports = {
-  dummy,
+  dummy, totalLikes
 };
