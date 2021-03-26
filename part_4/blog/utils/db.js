35f39afbafe3aad902bd3551408604dbcc56a99d
Tragikomedia <kamilk9625@gmail.com>
2021-03-26 19:06:05 +0100

    Server starts.

---
+++ part_4/blog/utils/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const config = require('./config');
+const logger = require('./logger');
+
+const connect = async () => {
+  try {
+    await mongoose.connect(config.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }, () => logger.info('Connected to DB'));
+  } catch (error) {
+    logger.error(error);
+  }
+};
+
+module.exports = { connect };
