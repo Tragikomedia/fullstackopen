7ea6bb6ac0151056b854e55390b84bb6bc2ce09c
Tragikomedia <kamilk9625@gmail.com>
2021-03-31 18:32:37 +0200

    Finished 5.14.

--- part_5/bloglist/src/components/Blog.test.js
+++ part_5/bloglist/src/components/Blog.test.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Blog from './Blog';
 
 describe('Blog', () => {
@@ -25,13 +25,25 @@ describe('Blog', () => {
     );
   });
 
-  it('Should only show blog\'s title and author initially', () => {
+  it('Given initial state, should only show blog\'s title, author and button to show details', () => {
     const blogElt = component.container.querySelector('.blog');
     expect(blogElt).toHaveTextContent(blog.title);
     expect(blogElt).toHaveTextContent(blog.author);
     expect(blogElt).not.toHaveTextContent(blog.url);
-    expect(blogElt).not.toHaveTextContent(blog.id);
+    expect(blogElt).not.toHaveTextContent(blog.likes);
     const button = component.container.querySelector('button');
     expect(button).toHaveTextContent('Expand');
+  });
+
+  it('Given the expanding button\'s been clicked, should show detailed info', () => {
+    const blogElt = component.container.querySelector('.blog');
+    const button = component.container.querySelector('button');
+    fireEvent.click(button);
+    expect(blogElt).toHaveTextContent(blog.title);
+    expect(blogElt).toHaveTextContent(blog.author);
+    expect(blogElt).toHaveTextContent(blog.url);
+    expect(blogElt).toHaveTextContent(blog.likes);
+    const buttons = component.container.querySelectorAll('button');
+    expect(buttons.length).toBe(3);
   });
 });
