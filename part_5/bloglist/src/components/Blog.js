1c2db698b9885c0b15a02ba612b25fa628c90c56
Tragikomedia <kamilk9625@gmail.com>
2021-03-31 12:32:50 +0200

    Finished 5.7.

--- part_5/bloglist/src/components/Blog.js
+++ part_5/bloglist/src/components/Blog.js
@@ -1,7 +1,27 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [expand, setExpand] = useState(false);
+
+  return (
+    <div className="blog">
+      <ul>
+        <li>
+          {blog.title} {blog.author}
+          <button onClick={() => setExpand(!expand)}>
+            {expand ? "Hide" : "Expand"}
+          </button>
+        </li>
+        {expand && (
+          <>
+            <li>{blog.url}</li>
+            <li>{`likes ${blog.likes}`} <button>Like</button></li>
+            <li>{blog.user.name}</li>
+          </>
+        )}
+      </ul>
+    </div>
+  );
+};
 
 export default Blog;
