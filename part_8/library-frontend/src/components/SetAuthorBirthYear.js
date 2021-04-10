7ccd3f5aab716e86992c26782b421aae189a1296
Tragikomedia <kamilk9625@gmail.com>
2021-04-10 13:01:20 +0200

    Finished 8.12.

--- part_8/library-frontend/src/components/SetAuthorBirthYear.js
+++ part_8/library-frontend/src/components/SetAuthorBirthYear.js
@@ -1,5 +1,6 @@
 import { useMutation } from "@apollo/client";
 import { useState } from "react";
+import { useQuery } from '@apollo/client';
 import { EDIT_AUTHOR } from "../data/authors/mutation";
 import { ALL_AUTHORS } from "../data/authors/query";
 import React from "react";
@@ -8,9 +9,12 @@ const SetAuthorBirthYear = () => {
   const [name, setName] = useState("");
   const [year, setYear] = useState("");
 
+  const {data} = useQuery(ALL_AUTHORS);
   const [editBirthYear] = useMutation(EDIT_AUTHOR, {
     refetchQueries: [{ query: ALL_AUTHORS }],
   });
+
+  const authors = data?.allAuthors ?? [];
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -29,12 +33,9 @@ const SetAuthorBirthYear = () => {
       <h3>Set birthyear</h3>
       <form onSubmit={onSubmit}>
         <div>
-          Name
-          <input
-            type="text"
-            value={name}
-            onChange={({ target }) => setName(target.value)}
-          />
+          <select value={name} onChange={({target}) => setName(target.value)}>
+            {authors.map(author => <option key={author.id} value={author.name}>{author.name}</option>)}
+          </select>
         </div>
         <div>
           Year{" "}
