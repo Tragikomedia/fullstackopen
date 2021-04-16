53402ace5b1c78974b19113834aa8fcadfbd1af0
Tragikomedia <kamilk9625@gmail.com>
2021-04-16 14:04:00 +0200

    Finished 9.9.

--- part_9/patientor-backend/index.ts
+++ part_9/patientor-backend/index.ts
@@ -1,14 +1,17 @@
 import express from "express";
+import cors from 'cors';
 
 const app = express();
 
-app.use(express.json());
+//eslint-disable-next-line
+app.use(cors()); 
+app.use(express.json()); 
 
-app.get("/ping", (_req, res) => {
+app.get("/api/ping", (_req, res) => {
   res.send("PONG");
 });
 
-const PORT = 3003;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}`);
